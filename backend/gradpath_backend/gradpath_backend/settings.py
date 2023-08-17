72c50a89c12ac2caa457f01ddab38dd389cea716
Ubuntu <ubuntu@ip-172-31-9-249.us-east-2.compute.internal>
2023-08-17 22:04:39 +0000

    move changes made on aws to github

--- backend/gradpath_backend/gradpath_backend/settings.py
+++ backend/gradpath_backend/gradpath_backend/settings.py
@@ -24,11 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fei110o23$ubsvf0v&q2b_z%)!uw-a2ja8+wfk4(20&6kzneji'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '3.144.111.173',
+]
 
-load_dotenv()
+load_dotenv(dotenv_path=BASE_DIR / '.env')
 
 db_engine = os.getenv('DB_ENGINE')
 
@@ -146,6 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
