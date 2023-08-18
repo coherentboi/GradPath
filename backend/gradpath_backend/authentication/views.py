edda1de1a5f723bb6b6a8ef9bc8d1045a210c6ac
876pol <chenpaul.pc@gmail.com>
2023-08-18 18:25:41 -0400

    existing_password -> current password, remove email change functionality

--- backend/gradpath_backend/authentication/views.py
+++ backend/gradpath_backend/authentication/views.py
@@ -46,27 +46,22 @@ class EditProfileView(UpdateAPIView):
         profile = user.profile
 
         # Verify existing password
-        existing_password = request.data.get('existing_password')
-        if not existing_password or not user.check_password(existing_password):
-            return Response({"error": "Existing password is incorrect."}, status=status.HTTP_400_BAD_REQUEST)
+        current_password = request.data.get('current_password')
+        if not current_password or not user.check_password(current_password):
+            return Response({"error": "Current password is incorrect."}, status=status.HTTP_400_BAD_REQUEST)
 
-        # Check if the new username and email are unique
+        # Check if the new username is unique
         new_username = request.data.get('username')
-        new_email = request.data.get('email')
         if new_username and User.objects.filter(username=new_username).exclude(pk=user.pk).exists():
             return Response({"error": "Username already exists."}, status=status.HTTP_400_BAD_REQUEST)
-        if new_email and User.objects.filter(email=new_email).exclude(pk=user.pk).exists():
-            return Response({"error": "Email already exists."}, status=status.HTTP_400_BAD_REQUEST)
 
-        # Update other profile fields (except has_paid)
+        # Update other profile fields (except has_paid and email)
         serializer = ProfileSerializer(profile, data=request.data, partial=True)
         if serializer.is_valid():
 
-            # Update username and email if provided
+            # Update username if provided
             if new_username:
                 user.username = new_username
-            if new_email:
-                user.email = new_email
 
             # Update password if provided
             password = request.data.get('password')
