d51408017346cb8e72a10dc336a4644deed7bd13
root <root@ip-172-31-9-249.us-east-2.compute.internal>
2023-08-24 01:47:15 +0000

    async email fix

--- backend/gradpath_backend/registration_form/views.py
+++ backend/gradpath_backend/registration_form/views.py
@@ -1,14 +1,21 @@
-import asyncio
-import logging
-
-from asgiref.sync import async_to_sync
+import threading
 from django.core.mail import send_mail
-from rest_framework.generics import CreateAPIView
-
 from gradpath_backend import settings
+from rest_framework.generics import CreateAPIView
 from .models import RegistrationForm
 from .serializers import RegistrationFormSerializer
 
+def send_email():
+    try:
+        send_mail(
+            'Someone submitted the registration form!',
+            'View the details here at https://gradpathtutoring.com/api/admin',
+            settings.EMAIL_HOST_USER,
+            [settings.EMAIL_HOST_USER],
+            fail_silently=True,
+        )
+    except Exception as e:
+        logging.error(f'Failed to send email: {e}')
 
 class RegistrationFormCreateView(CreateAPIView):
     queryset = RegistrationForm.objects.all()
@@ -16,19 +23,6 @@ class RegistrationFormCreateView(CreateAPIView):
 
     def perform_create(self, serializer):
         instance = serializer.save()
-        async_to_sync(self.send_email_async)()
+        thread = threading.Thread(target=send_email)
+        thread.start()  # This will start the email sending in a separate thread
 
-    async def send_email_async(self):
-        await asyncio.get_running_loop().run_in_executor(None, self.send_email)
-
-    def send_email(self):
-        try:
-            send_mail(
-                'Someone submitted the registration form!',
-                'View the details here at https://gradpathtutoring.com/api/admin',
-                settings.EMAIL_HOST_USER,
-                [settings.EMAIL_HOST_USER],
-                fail_silently=True,
-            )
-        except Exception as e:
-            logging.error(f'Failed to send email: {e}')
