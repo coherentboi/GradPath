d136007be35fd3b7d1e6765dcbacde64550bbf04
876pol <chenpaul.pc@gmail.com>
2023-08-23 21:22:05 -0400

    python backend async

--- backend/gradpath_backend/registration_form/views.py
+++ backend/gradpath_backend/registration_form/views.py
@@ -1,3 +1,7 @@
+import asyncio
+import logging
+
+from asgiref.sync import async_to_sync
 from django.core.mail import send_mail
 from rest_framework.generics import CreateAPIView
 
@@ -12,11 +16,19 @@ class RegistrationFormCreateView(CreateAPIView):
 
     def perform_create(self, serializer):
         instance = serializer.save()
+        async_to_sync(self.send_email_async)()
 
-        send_mail(
-            'Someone submitted the registration form!',
-            'View the details here at https://gradpathtutoring.com/api/admin',
-            settings.EMAIL_HOST_USER,  # Sender's email address, taken from settings.py
-            [settings.EMAIL_HOST_USER],
-            fail_silently=False,
-        )
+    async def send_email_async(self):
+        await asyncio.get_running_loop().run_in_executor(None, self.send_email)
+
+    def send_email(self):
+        try:
+            send_mail(
+                'Someone submitted the registration form!',
+                'View the details here at https://gradpathtutoring.com/api/admin',
+                settings.EMAIL_HOST_USER,
+                [settings.EMAIL_HOST_USER],
+                fail_silently=True,
+            )
+        except Exception as e:
+            logging.error(f'Failed to send email: {e}')
